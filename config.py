f225651835085d41b2bf4c36f89c5c5e0da9a36c
Sterga <42569896+Sterga@users.noreply.github.com>
2020-07-11 21:33:09 +0200

    Initial Commit

--- config.py
+++
@@ -1,7 +0,0 @@
-ENDPOINT = "https://integrationia.cognitiveservices.azure.com/"
-prediction_key = "38c8c5437a5a41788425ed7ccd2d8835"
-publish_iteration_name = "classifyModel"
-project_id = "25e3b8c7-801d-4ebf-930f-7140eebc8263"
-
-# Define project path
-base_image_url = r"C:\Users\rahmedali\PycharmProjects\triof"
